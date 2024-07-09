f56c2546b37ff453fcf424dcbdcfe27f3da59a13
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-07-09 15:00:59 +0530

    basic structure made

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import "./Home.css"
+import Insert from "./plus.png"
 
 function Home() {
-  return (
+  return (<>
     <div>
-      <h1>To-Do App
-      </h1>
+      <h1 className="app-heading">To-Do App</h1> 
+      <div className="todo-app-container">
+
+      </div>
+
+      <div className="input-container">
+      <input type="text" className='task-input'></input>
+    
+        <img src={Insert} className="insert-button" alt="Insert" onClick={}/>
+
+      </div>
     </div>
-  )
+    </>)
 }
 
 export default Home
